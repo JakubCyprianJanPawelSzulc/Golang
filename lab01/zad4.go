2f5fa4d6f17b5ef03ea9814ca8f10df66a47d617
Jakub Szulc <j.szulc.328@studms.ug.edu.pl>
2023-02-24 14:50:05 +0100

    zad4poziom3

--- lab01/zad4.go
+++ lab01/zad4.go
@@ -1,10 +1,12 @@
-//Gra w Zgadywanie
-//Trudność: zależnie od poziomu zadania.
-//Napisz program, którego zadaniem jest przeprowadzić grę z człowiekiem, w zgadywanie wylosowanej przez komputer liczby. Zadanie można zrobić prosto, lub w sposób bardzo rozbudowany. Warianty, a właściwie "poziomy" przedstawione są poniżej.
-//Poziom 1.
-//Program na przedstawić użytkownikowi zadanie, np. "Teraz będziesz zgadywać liczbę, którą wylosowałem" - a następnie zapytać użytkownika o tę liczbę, np. pisząc "Podaj liczbę: ", wczytać ją, i sprawdzić, czy wylosowana wcześniej liczba jest taka sama. Jeżeli tak, mają się pojawić gratulacje, a program się kończy. Jeżeli nie, program powinien napisać czy liczba podana przez użytkownika jest "Za mała", lub "Za duża". Wtedy powtarzane są pytania aż w końcu użytkownik zgadnie lub przerwie program.
-//Poziom 2.
-//Napisz program jak wyżej, ale zmodyfikuj go w taki sposób, aby możliwe było podanie odpowiedzi "koniec", po której pytania o liczbę są przerywane a program natychmiast się kończy pisząc "żegnaj". Program powinien w powitaniu napisać informację, że po wpisaniu "koniec" nastąpi zakończenie działania. Poza tym - wszystko może pozostać takie, jak na poziomie pierwszym.
+// Gra w Zgadywanie
+// Trudność: zależnie od poziomu zadania.
+// Napisz program, którego zadaniem jest przeprowadzić grę z człowiekiem, w zgadywanie wylosowanej przez komputer liczby. Zadanie można zrobić prosto, lub w sposób bardzo rozbudowany. Warianty, a właściwie "poziomy" przedstawione są poniżej.
+// Poziom 1.
+// Program na przedstawić użytkownikowi zadanie, np. "Teraz będziesz zgadywać liczbę, którą wylosowałem" - a następnie zapytać użytkownika o tę liczbę, np. pisząc "Podaj liczbę: ", wczytać ją, i sprawdzić, czy wylosowana wcześniej liczba jest taka sama. Jeżeli tak, mają się pojawić gratulacje, a program się kończy. Jeżeli nie, program powinien napisać czy liczba podana przez użytkownika jest "Za mała", lub "Za duża". Wtedy powtarzane są pytania aż w końcu użytkownik zgadnie lub przerwie program.
+// Poziom 2.
+// Napisz program jak wyżej, ale zmodyfikuj go w taki sposób, aby możliwe było podanie odpowiedzi "koniec", po której pytania o liczbę są przerywane a program natychmiast się kończy pisząc "żegnaj". Program powinien w powitaniu napisać informację, że po wpisaniu "koniec" nastąpi zakończenie działania. Poza tym - wszystko może pozostać takie, jak na poziomie pierwszym.
+// Poziom 3.
+// Napisz program taki jak na poziomie 2, ale dodaj do niego dodatkowe pytanie na końcu, po odgadnięciu liczby, które brzmi "Czy gramy jeszcze raz? [T/N]". Gdy użytkownik wybierze odpowiedź (domyślnie tak lub nie), program rozpoczyna jeszcze raz grę z INNĄ wylosowaną liczbą, albo się kończy.
 
 package main
 
@@ -17,34 +19,46 @@ import (
 )
 
 func main() {
-	fmt.Println("Podaj maksimum zakresu")
-	var max int
-	fmt.Scanln(&max)
 	rand.Seed(time.Now().UnixNano())
-	number := rand.Intn(max)
-	fmt.Println("Zgadnij liczbę z zakresu 0 -", max, "lub wpisz 'koniec' aby zakończyć grę")
 
-	var guess string
 	for {
-		fmt.Scanln(&guess)
-		if strings.ToLower(guess) == "koniec" {
+		fmt.Println("Podaj maksimum zakresu")
+		var max int
+		fmt.Scanln(&max)
+
+		number := rand.Intn(max)
+		fmt.Println("Zgadnij liczbę z zakresu 0 -", max, "lub wpisz 'koniec' aby zakończyć grę")
+
+		var guess string
+		for {
+			fmt.Scanln(&guess)
+			if strings.ToLower(guess) == "koniec" {
+				fmt.Println("Żegnaj!")
+				return
+			}
+
+			guessNum, err := strconv.Atoi(guess)
+			if err != nil {
+				fmt.Println("To nie jest liczba, spróbuj ponownie lub wpisz 'koniec'")
+				continue
+			}
+
+			if guessNum == number {
+				fmt.Println("Zgadłeś!")
+				break
+			} else if guessNum > number {
+				fmt.Println("Za dużo")
+			} else {
+				fmt.Println("Za mało")
+			}
+		}
+
+		var playAgain string
+		fmt.Println("Czy gramy jeszcze raz? [T/N]")
+		fmt.Scanln(&playAgain)
+		if strings.ToLower(playAgain) != "t" {
 			fmt.Println("Żegnaj!")
-			break
-		}
-
-		guessNum, err := strconv.Atoi(guess)
-		if err != nil {
-			fmt.Println("To nie jest liczba, spróbuj ponownie lub wpisz 'koniec'")
-			continue
-		}
-
-		if guessNum == number {
-			fmt.Println("Zgadłeś!")
-			break
-		} else if guessNum > number {
-			fmt.Println("Za dużo")
-		} else {
-			fmt.Println("Za mało")
+			return
 		}
 	}
 }
