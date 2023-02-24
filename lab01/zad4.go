7d3f11b31a3b5d7c0e875ed973a73027b17d7bb4
Jakub Szulc <j.szulc.328@studms.ug.edu.pl>
2023-02-24 15:48:04 +0100

    zad4poziom4

--- lab01/zad4.go
+++ lab01/zad4.go
@@ -7,19 +7,29 @@
 // Napisz program jak wyżej, ale zmodyfikuj go w taki sposób, aby możliwe było podanie odpowiedzi "koniec", po której pytania o liczbę są przerywane a program natychmiast się kończy pisząc "żegnaj". Program powinien w powitaniu napisać informację, że po wpisaniu "koniec" nastąpi zakończenie działania. Poza tym - wszystko może pozostać takie, jak na poziomie pierwszym.
 // Poziom 3.
 // Napisz program taki jak na poziomie 2, ale dodaj do niego dodatkowe pytanie na końcu, po odgadnięciu liczby, które brzmi "Czy gramy jeszcze raz? [T/N]". Gdy użytkownik wybierze odpowiedź (domyślnie tak lub nie), program rozpoczyna jeszcze raz grę z INNĄ wylosowaną liczbą, albo się kończy.
+// Poziom 4.
+// Program z poziomu 3 umożliwia wiele gier. Zmodyfikuj go tak, aby umożliwiał zapamiętanie w ilu próbach użytkownik odgadł liczbę. Po zakończeniu gry zapytaj użytkownika o imię, i zapisz je do struktury danych. Gdyby gra była kontynuowana kolejny raz, ponawiaj takie pytania, aby zebrać dane o wynikach różnych użytkowników. Na zakończenie programu wypisz podsumowanie: kto w ilu próbach odgadywał liczby, posortowane rosnąco według liczby prób.
 
 package main
 
 import (
 	"fmt"
 	"math/rand"
+	"sort"
 	"strconv"
 	"strings"
 	"time"
 )
 
+type playerResult struct {
+	name    string
+	guesses int
+}
+
 func main() {
 	rand.Seed(time.Now().UnixNano())
+
+	var results []playerResult
 
 	for {
 		fmt.Println("Podaj maksimum zakresu")
@@ -30,11 +40,12 @@ func main() {
 		fmt.Println("Zgadnij liczbę z zakresu 0 -", max, "lub wpisz 'koniec' aby zakończyć grę")
 
 		var guess string
+		var guesses int
 		for {
 			fmt.Scanln(&guess)
 			if strings.ToLower(guess) == "koniec" {
 				fmt.Println("Żegnaj!")
-				return
+				break
 			}
 
 			guessNum, err := strconv.Atoi(guess)
@@ -43,6 +54,7 @@ func main() {
 				continue
 			}
 
+			guesses++
 			if guessNum == number {
 				fmt.Println("Zgadłeś!")
 				break
@@ -53,12 +65,29 @@ func main() {
 			}
 		}
 
+		if guesses > 0 {
+			var name string
+			fmt.Println("Podaj swoje imię")
+			fmt.Scanln(&name)
+
+			results = append(results, playerResult{name, guesses})
+		}
+
 		var playAgain string
-		fmt.Println("Czy gramy jeszcze raz? [T/N]")
+		fmt.Println("Chcesz zagrać jeszcze raz? [T/N]")
 		fmt.Scanln(&playAgain)
 		if strings.ToLower(playAgain) != "t" {
 			fmt.Println("Żegnaj!")
-			return
+			break
 		}
+	}
+
+	sort.Slice(results, func(i, j int) bool {
+		return results[i].guesses < results[j].guesses
+	})
+
+	fmt.Println("Podsumowanie wyników:")
+	for _, result := range results {
+		fmt.Printf("%s odgadł(a) w %d próbach\n", result.name, result.guesses)
 	}
 }
